d588642c01a4db350b9c314879dbf2733d98b721
Subash Chandra <subashc2023@gmail.com>
2024-12-28 22:45:49 -0600

    Update CleanupSimulator.tsx

--- src/components/CleanupSimulator.tsx
+++ src/components/CleanupSimulator.tsx
@@ -498,7 +498,7 @@ const calculateZeroYear = (latestData: DataPoint | undefined): number | null => 
                     value: 'Total Accumulated Plastic (Million Tons)', 
                     angle: 90, 
                     position: 'outside',
-                    offset: 45,
+                    offset: -20,
                     style: { fill: '#9CA3AF' }
                   }}
                   tickMargin={0}
