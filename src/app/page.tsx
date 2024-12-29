b8d2d4c53012bd80670503e6a8a7af5e221946a8
Subash Chandra <subashc2023@gmail.com>
2024-12-28 21:24:38 -0600

    last few whites

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import CleanupSimulator from '../components/CleanupSimulator';
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gray-50">
+    <main className="min-h-screen bg-[#0d1117]">
       <CleanupSimulator/>
     </main>
   );
