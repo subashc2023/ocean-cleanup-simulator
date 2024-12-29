d5e17eeb452a9dce4db707d3a79da67cf4b841e3
Subash Chandra <subashc2023@gmail.com>
2024-12-28 22:26:57 -0600

    Update CleanupSimulator.tsx

--- src/components/CleanupSimulator.tsx
+++ src/components/CleanupSimulator.tsx
@@ -485,10 +485,10 @@ const calculateZeroYear = (latestData: DataPoint | undefined): number | null => 
                     value: 'Daily Plastic Flow (Thousands of Metric Tons/Day)', 
                     angle: -90, 
                     position: 'outside',
-                    offset: 0,
+                    offset: -20,
                     style: { fill: '#9CA3AF' }
                   }}
-                  tickMargin={8}
+                  tickMargin={16}
                   tickFormatter={(value) => (value / 1000).toFixed(0)}
                 />
                 <YAxis 
@@ -498,10 +498,10 @@ const calculateZeroYear = (latestData: DataPoint | undefined): number | null => 
                     value: 'Total Accumulated Plastic (Million Tons)', 
                     angle: 90, 
                     position: 'outside',
-                    offset: 0,
+                    offset: -20,
                     style: { fill: '#9CA3AF' }
                   }}
-                  tickMargin={8}
+                  tickMargin={16}
                 />
                 <Tooltip 
                   formatter={(value: number, name) => {
