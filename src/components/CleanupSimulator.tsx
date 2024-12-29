8c659908437d0880ce3ab341a7d0756d6d19006b
Subash Chandra <subashc2023@gmail.com>
2024-12-28 20:31:24 -0600

    Update CleanupSimulator.tsx

--- src/components/CleanupSimulator.tsx
+++ src/components/CleanupSimulator.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useState, useEffect } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import { Card, CardContent } from '@/components/ui/card';
